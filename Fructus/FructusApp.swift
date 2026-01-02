ea5d9d9947c32826a2f1a1b66a25f77459e3e76b
Preeteesh Remalli <priteeshh@gmail.com>
2026-01-02 14:00:54 +0400

    Initial Commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Preeteesh Remalli on 02/01/26.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
